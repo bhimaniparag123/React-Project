6f35e6978517073bb409614bd6a5d69b5e5e4cb9
Parag Bhimani <159759757+bhimaniparag123@users.noreply.github.com>
2025-05-23 13:50:28 +0530

    update

--- pr-1-user-profile-app/src/App.js
+++ pr-1-user-profile-app/src/App.js
@@ -9,16 +9,19 @@ function App() {
         <UserProfileCard
           name="Jane Doe"
           email="jane.doe@example.com"
+          Age="25"
           profilePicture="https://randomuser.me/api/portraits/women/44.jpg"
           phone="+1234567890"
           address="123 Main St, Cityville"
           jobTitle="Frontend Developer"
           company="Tech Corp"
+          
         />
 
         <UserProfileCard
           name="John Smith"
           email="john.smith@test.in"
+          Age="30"
           profilePicture="https://randomuser.me/api/portraits/men/32.jpg"
           phone="+1987654321"
           address="456 Elm St, Townsville"
@@ -29,6 +32,7 @@ function App() {
         <UserProfileCard
           name="Alice Johnson"
           email="alice.j@test.in"
+          Age="28"
           profilePicture="https://randomuser.me/api/portraits/women/65.jpg"
           phone="+1122334455"
           address="789 Oak St, DevCity"
@@ -39,6 +43,7 @@ function App() {
         <UserProfileCard
           name="blindlyround Johnson"
           email="Johnson.b@gmail.com"
+          Age="58"
           profilePicture="https://randomuser.me/api/portraits/men/0.jpg"
           phone="+35465756242"
           address="469 kulu St, Orgej"
@@ -49,6 +54,7 @@ function App() {
         <UserProfileCard
           name="bidon each"
           email="bidon.each@test.in"
+          Age="79"
           profilePicture="https://randomuser.me/api/portraits/men/82.jpg"
           phone="+35465756242"
           address="469 kulu St, Orgej"
--- pr-1-user-profile-app/src/Components/UserProfileCard.js
+++ pr-1-user-profile-app/src/Components/UserProfileCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './UserProfileCard.css';
 
-const UserProfileCard = ({ name, email, profilePicture, phone, address, jobTitle, company, website }) => {
+const UserProfileCard = ({ name, email, Age, profilePicture, phone, address, jobTitle, company, website }) => {
   return (
     <div className="profile-card">
       <img src={profilePicture} alt={`${name}'s profile`} className="profile-picture" />
       <h2>{name}</h2>
       <p className="email"><strong>Email:</strong> {email}</p>
       <p><strong>Phone:</strong> {phone}</p>
+      <p><strong>Age:</strong> {Age}</p>
       <p><strong>Address:</strong> {address}</p>
       <p><strong>Job Title:</strong> {jobTitle}</p>
       <p><strong>Company:</strong> {company}</p>
